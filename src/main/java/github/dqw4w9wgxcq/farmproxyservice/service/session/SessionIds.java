9b26690301aea47ed0a3bd50a6757fb9c76af5da
dQw4w9WgXcQ <22358065+dQw4w9WgXcQ@users.noreply.github.com>
2022-11-24 22:12:57 -0800

    SessionIds

--- src/main/java/github/dqw4w9wgxcq/farmproxyservice/service/session/SessionIds.java
+++ src/main/java/github/dqw4w9wgxcq/farmproxyservice/service/session/SessionIds.java
@@ -2,11 +2,14 @@ package github.dqw4w9wgxcq.farmproxyservice.service.session;
 
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @Component
 public class SessionIds {
-    private volatile int counter = 0;
+    private static final long ORIGIN = Long.parseLong("10000000", 36);
+    private static final long BOUND = Long.parseLong("zzzzzzzz", 36) + 1;
 
-    public synchronized String generate() {
-        return Integer.toString(counter++);
+    public String generate() {
+        return Long.toString(ThreadLocalRandom.current().nextLong(ORIGIN, BOUND), 36);
     }
 }
