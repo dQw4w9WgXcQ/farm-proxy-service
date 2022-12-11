94a9f38fec3d71a44aefb9945c19d8b53a12e16b
dQw4w9WgXcQ <22358065+dQw4w9WgXcQ@users.noreply.github.com>
2022-12-11 11:30:42 -0800

    move repository classes into directories

--- src/main/java/github/dqw4w9wgxcq/farmproxyservice/repository/geo/GeoRepository.java
+++ src/main/java/github/dqw4w9wgxcq/farmproxyservice/repository/geo/GeoRepository.java
@@ -1,6 +1,5 @@
 package github.dqw4w9wgxcq.farmproxyservice.repository.geo;
 
-import github.dqw4w9wgxcq.farmproxyservice.repository.geo.Geo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
